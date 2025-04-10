324d82b79746313b31158b127d8385e6df6c3015
Kiranpreet Kaur LNU <klnu72@norquest.ca>
2025-04-09 19:26:43 -0600

    Update app.py

--- app.py
+++ app.py
@@ -24,12 +24,12 @@ index.add(doc_embeddings)
 # Dashboard Page
 def dashboard():
     st.markdown("""
-        <div style='text-align: center; width: 100%;'>
-            <img src='logo.jpeg' style='width: 100%; max-width: 300px;' />
-            <h1 style='color: #2E8B57;'>Welcome to IFSSA Client Retention Predictor</h1>
-            <p style='font-size:18px;'>Empowering community service with data-driven decisions</p>
-        </div>
-        """, unsafe_allow_html=True)
+    <div style='text-align: center; width: 100%;'>
+        <img src='logo.jpeg' style='width: 100%; max-width: 300px;' />
+        <h1 style='color: #2E8B57;'>Welcome to IFSSA Client Retention Predictor</h1>
+        <p style='font-size:18px;'>Empowering community service with data-driven decisions</p>
+    </div>
+    """, unsafe_allow_html=True)
 
     st.subheader("💡 Abstract")
     st.write("""
